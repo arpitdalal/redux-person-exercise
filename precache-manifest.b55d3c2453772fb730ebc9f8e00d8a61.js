b432c6441fc2672034457c1c8aa3b4a7befa6041
Arpit <arpitdalal21@gmail.com>
2020-08-02 12:03:02 -0300

    Updates

---
+++ precache-manifest.b55d3c2453772fb730ebc9f8e00d8a61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d09613f9f34f124ff05bca154850a0",
+    "url": "/redux-person-exercise/index.html"
+  },
+  {
+    "revision": "c296d3a427ff8db27a98",
+    "url": "/redux-person-exercise/static/css/main.d14221ff.chunk.css"
+  },
+  {
+    "revision": "25fd9a9ac9102bce5a3e",
+    "url": "/redux-person-exercise/static/js/2.7d29a664.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-person-exercise/static/js/2.7d29a664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c296d3a427ff8db27a98",
+    "url": "/redux-person-exercise/static/js/main.ba0b9f02.chunk.js"
+  },
+  {
+    "revision": "beeb3b3ec80df3dbeb41",
+    "url": "/redux-person-exercise/static/js/runtime-main.9683cd03.js"
+  }
+]);
